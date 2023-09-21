8c87b2254b375fa5fa7ce76b2129142d64433d7c
mkronvold <mkronvold@dev2013.dev.e2open.com>
2023-09-21 18:11:34 +0000

    cleanup

--- dev/post-install/install-contour.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-tanzu package install cert-manager  --package-name cert-manager.tanzu.vmware.com --version 1.7.2+vmware.1-tkg.1 -n cert-manager --create-namespace
-
-tanzu package install contour  --package-name contour.tanzu.vmware.com --version 1.20.2+vmware.1-tkg.1 -n contour --create-namespace --values-file contour-default-values.yaml
-
-kubectl get pods -n tanzu-system-ingress 
